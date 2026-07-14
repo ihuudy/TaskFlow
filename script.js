63eae70619491713adc3fe2fd6fca098125c6390
Hudson de Souza Santos <hudsonsouzasantos33@gmail.com>
2026-07-14 18:56:05 -0300

    feat: função de adicicionar elementos ao HTML via JS

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
-console.log("TaskFlow iniciado!");
-
-const button = document.querySelector(".btn");
+const buttonAdd = document.querySelector(".btn_add");
 const addTask = document.querySelector("#add_tarefa");
+const taskList = document.querySelector(".list_tarefas");
 
-button.addEventListener("click", function novaTarefa() {
-    console.log("tarefa adicionada");
+buttonAdd.addEventListener("click", function novaTarefa(event) {
     event.preventDefault();
-});
+    const tarefa = addTask.value.trim();
+    if (!tarefa) {
+        console.log("Digite uma tarefa!");
+    } else {
+        const article = document.createElement("article");
+        const label = document.createElement("label")
+        const checkbox = document.createElement("input")
 
-console.log(button);
-console.log(addTask);
+        article.classList.add("cards");
+        label.textContent = tarefa;
+        checkbox.type = "checkbox";
+
+        article.appendChild(checkbox);
+        article.appendChild(label);
+        taskList.appendChild(article);
+        console.log(article);
+        
+    }
+});
