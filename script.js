f0024c683e54963ed6ab2b473e2c0152509b7e99
Hudson de Souza Santos <hudsonsouzasantos33@gmail.com>
2026-07-14 16:05:47 -0300

    feat: inicio da lóogica de adiconar tarefas

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 console.log("TaskFlow iniciado!");
+
+const button = document.querySelector(".btn");
+const addTask = document.querySelector("#add_tarefa");
+
+button.addEventListener("click", function novaTarefa() {
+    console.log("tarefa adicionada");
+    event.preventDefault();
+});
+
+console.log(button);
+console.log(addTask);
