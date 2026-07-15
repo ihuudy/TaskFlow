e7eca1c9ecce92d375d8923d502b52912915826e
Hudson de Souza Santos <hudsonsouzasantos33@gmail.com>
2026-07-15 14:38:35 -0300

    feat: refratorar codigo js 1

--- script.js
+++ script.js
@@ -1,25 +1,40 @@
+const tarefas = [];
+
 const buttonAdd = document.querySelector(".btn_add");
 const addTask = document.querySelector("#add_tarefa");
 const taskList = document.querySelector(".list_tarefas");
 
-buttonAdd.addEventListener("click", function novaTarefa(event) {
+buttonAdd.addEventListener("click", novaTarefa);
+
+function novaTarefa(event) {
     event.preventDefault();
     const tarefa = addTask.value.trim();
     if (!tarefa) {
         console.log("Digite uma tarefa!");
-    } else {
+        return;
+    }
+
+    criarCard(tarefa);
+    tarefas.push(tarefa);
+}
+
+function criarCard(tarefa) {
+
         const article = document.createElement("article");
         const label = document.createElement("label")
         const checkbox = document.createElement("input")
 
         article.classList.add("cards");
-        label.textContent = tarefa;
         checkbox.type = "checkbox";
+        label.textContent = tarefa;
 
         article.appendChild(checkbox);
         article.appendChild(label);
+
         taskList.appendChild(article);
-        console.log(article);
-        
-    }
-});
+
+        addTask.value = "";
+        addTask.focus();
+        console.log(tarefas);
+
+}
